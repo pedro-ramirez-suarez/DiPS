769232eb9865c90b0f02dc7580821f5a5b99f8e8
Pedro Ramirez <pramirez@sciodev.com>
2015-07-07 16:45:31 -0500

    removed blanks

--- DiPS/Program.cs
+++ DiPS/Program.cs
@@ -16,23 +16,14 @@ namespace DiPS
         static void Main(string[] args)
         {
 
-
-            // running as service
-            //using (var service = new DiPSService())
-            //    ServiceBase.Run(service);
             DiPSService.ServiceStart();
-
-
             Console.WriteLine("DiPS Server");
-
             Console.WriteLine("Listening on port {0}, type Ctr+C' to stop the service", ConfigurationManager.AppSettings["port"]);
-
             Console.CancelKeyPress += (sender, eArgs) =>
             {
                 _quit.Set();
                 eArgs.Cancel = true;
             };
-
             _quit.WaitOne();
         }
 
