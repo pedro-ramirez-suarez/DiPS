aebd43d4a537e25c622dc9a7747f100d57674551
Pedro Ramirez <pedro.rmrz.srz@gmail.com>
2015-08-20 12:18:57 -0500

    small change in comment

--- DiPS/Program.cs
+++ DiPS/Program.cs
@@ -11,14 +11,14 @@ namespace DiPS
 {
     class Program
     {
-        //This is to run the app as a background service(not a windows service)
+        //This is to run the app as a background process(not a windows service)
         static ManualResetEvent _quit = new ManualResetEvent(false);
         static void Main(string[] args)
         {
             ///The service will start on the port and url specified on the .config file
             DiPSService.ServiceStart();
             Console.WriteLine("DiPS Server");
-            Console.WriteLine("Listening on port {0}, type Ctr+C' to stop the service", ConfigurationManager.AppSettings["port"]);
+            Console.WriteLine("Listening on port {0}, type Ctr + C' to stop the service", ConfigurationManager.AppSettings["port"]);
             Console.CancelKeyPress += (sender, eArgs) =>
             {
                 _quit.Set();
