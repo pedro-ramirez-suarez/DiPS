5b2b4a8dcac830f66d9ce3b00dd3ff4956ca4083
Pedro Ramirez <pedro.rmrz.srz@gmail.com>
2015-12-01 16:35:42 -0600

    DiPS Backend application ready

---
+++ DiPSBackEndApplication/Application/DipsBackEnd.cs
@@ -0,0 +1,122 @@
+﻿using DiPS.Client;
+using DiPSBackEndApplication.Controller;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiPSBackEndApplication.Application
+{
+    /// <summary>
+    /// A DiPS Application initializer
+    /// Setup the connection with DiPS and registers all the controller's public methods
+    /// Initialize the backend calling the Initialize backend, for a console app, create a static constructor like this:
+    /// static Program() 
+    /// {    
+    ///     InitializeBackEnd();
+    /// }
+    /// </summary>
+    public class DiPSBackEnd
+    {
+        /// <summary>
+        /// The DiPS service
+        /// </summary>
+        static DiPSClient Client;
+
+        /// <summary>
+        /// Static ctor
+        /// Registers all DiPSControllers
+        /// </summary>
+        public static void InitializeBackEnd(bool initializeServer = true)
+        {
+            if (initializeServer)
+                InitializeDiPS();
+            Client = new DiPSClient(ConfigurationManager.AppSettings["dipsserver"]);
+            RegisterAllControllers();
+        }
+
+
+        /// <summary>
+        /// Static ctor
+        /// Registers all DiPSControllers
+        /// </summary>
+        public static void InitializeBackEnd(string dipsConnectionString, bool initializeServer = true)
+        {
+            if (initializeServer)
+                InitializeDiPS();
+            Client = new DiPSClient(dipsConnectionString);
+            RegisterAllControllers();
+        }
+
+        /// <summary>
+        /// Launch the DiPS service if is not running
+        /// </summary>
+        private static void InitializeDiPS()
+        {
+            var dipsRunning = false;
+            var dipsLaunched = false;
+            while (true)
+            {
+
+                Process[] processlist = Process.GetProcesses();
+                foreach (Process theprocess in processlist)
+                {
+                    if (theprocess.ProcessName.ToLower().Trim().Contains("dips"))
+                    {
+                        dipsRunning = true;
+                        Console.Clear();
+                        break;
+                    }
+                }
+                if (dipsRunning)
+                    break;
+                else if (!dipsLaunched)
+                {
+                    //attempt to start DiPS
+                    Process.Start(ConfigurationManager.AppSettings["dipsserverpath"] + "\\DiPS.exe");
+                    //sleeps a few milisecs
+                    Thread.Sleep(1000);
+                    dipsLaunched = true;
+                    continue;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Seems like the DiPS server is not running, please make sure that the server is running.");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Press enter to retry, or another key to exit");
+                if (Console.ReadKey().Key != ConsoleKey.Enter)
+                    Environment.Exit(1);
+            }
+        }
+
+
+        /// <summary>
+        /// Register all controllers found in the assembly
+        /// </summary>
+        private static void RegisterAllControllers()
+        {
+            var allTypes = Assembly.GetEntryAssembly().GetTypes();
+            foreach (var t in allTypes)
+            {
+                if (t.BaseType == typeof(DiPSController))
+                {
+                    try
+                    {
+                        dynamic controller = Activator.CreateInstance(t, Client);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(e);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DiPSBackEndApplication/Controller/DiPSController.cs
@@ -0,0 +1,55 @@
+﻿using DiPS.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiPSWebSockets.Server;
+using System.Threading;
+using DiPS.Client;
+
+namespace DiPSBackEndApplication.Controller
+{
+    /// <summary>
+    /// Move this to DiPS
+    /// </summary>
+    public abstract class DiPSController
+    {
+
+        protected DiPSClient DiPSClient { get; private set; }
+        /// <summary>
+        /// Ctor, we need the client to subscribe all the events in here
+        /// </summary>
+        public DiPSController(DiPSClient client)
+        {
+            DiPSClient = client;
+            var myType = this.GetType();
+            //get all the methods
+            var methods = myType.GetMethods();
+            foreach (var m in methods)
+            {
+                //register only public methods
+                if (!m.IsPublic)
+                    continue;
+
+                client.Subscribe(myType.Name + "." + m.Name, (param) =>
+                {
+                    try
+                    {
+                        var pars = new List<object>();
+                        pars.Add(param);
+                        m.Invoke(this, pars.ToArray());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(e);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                });
+            }
+        }
+
+
+    }
+}
